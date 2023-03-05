f140c4e150c18094bd41698a07197c47509a6dd0
hakeem <hakeem0114@gmail.com>
2023-03-05 18:12:11 -0500

    Finished factorial

--- main.js
+++ main.js
@@ -6,16 +6,57 @@ console.log("Console Log is connected")
 // array = [0,1]
 //Next number is the sum of the previous 2
 
-// fibonnacci(7) = [0,1,1,2,3,5,8])
+// // fibonnacci(7) = [0,1,1,2,3,5,8])
 
-function fibonnacci(n){
-    let fib = [0,1];
+// function fibonnacci(n){
+//     let fib = [0,1];
 
-    for(let i = 2;i<n;i++){
-        fib[i] = fib[i-1] + fib[i-2];
-    }
-    return fib;
-}
+//     for(let i = 2;i<n;i++){
+//         fib[i] = fib[i-1] + fib[i-2]; //New number = sum of (past 2 numbers)
+
+//     }
+//     return fib;
+// }
 
 
-console.log(fibonnacci(7));
+// console.log(fibonnacci(7));
+
+//Big O = O(n)
+//Space Complexity = O(n) since you are adding more memory to the array per iteration
+
+//#2: Factorial of a number
+/*
+Given an int n, find the factorial
+Factorial is the product of positive int <= n
+Fact(0) = 1
+// Fact(5) = 5*4*3*2*1 = 120
+// */
+
+// function Factorial(n){
+//     if(n == 0){
+//         return 1
+//     }else{
+//         return n*Factorial(n-1)
+//     } 
+// }
+// console.log(Factorial(5))
+// console.log(Factorial(0))
+
+// /*
+// Big O: O(n) since the time changes based on the increasing n
+
+// Space Complexity: O(n)
+// */
+
+//#3: Prime Number
+/*
+
+*/
+
+
+
+// /*
+// Big O: 
+
+// Space Complexity: 
+// */
